7df1dbdde9b6d78ca140c151cdbe56f9abfe5a6d
codecaiine <akabrouyannickn@gmail.com>
2021-11-04 23:19:02 +0000

    Add different queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
-/*Queries that provide answers to the questions from all projects.*/
 SELECT * from animals WHERE animal_name like '%mon';
 SELECT animal_name FROM animals WHERE date_of_birth BETWEEN '2016.01.01' AND '2019.12.31';
 SELECT animal_name FROM animals WHERE neutered='1' AND escape_attempts < 3;
@@ -7,15 +6,12 @@ SELECT animal_name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered='1';
 SELECT * FROM animals WHERE animal_name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= '10.4' AND weight_kg <= '17.3';
-
 ALTER TABLE animals
 ADD species VARCHAR(250);
-
 BEGIN;
 UPDATE animals
 SET species='unspecified';
 ROLLBACK;
-
 BEGIN;
 UPDATE animals
 SET species='digimon'
@@ -24,11 +20,9 @@ UPDATE animals
 SET species='pokemon'
 WHERE species='';
 COMMIT;
-
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
-
 BEGIN;
 DELETE FROM animals
 WHERE date_of_birth > '2022.01.01';
@@ -40,7 +34,6 @@ UPDATE animals
 SET weight_kg= weight_kg * '-1'
 WHERE weight_kg < 0;
 COMMIT;
-
 SELECT FROM animals;
 SELECT FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
@@ -52,3 +45,30 @@ SELECT MIN(weight_kg) FROM animals WHERE species='pokemon';
 SELECT MAX(weight_kg) FROM animals WHERE species='pokemon';
 SELECT AVG(escape_attempts) FROM animals WHERE (species='digimon') AND(date_of_birth BETWEEN '1990.01.01' AND '2000.12.31');
 SELECT AVG(escape_attempts) FROM animals WHERE (species='pokemon') AND(date_of_birth BETWEEN '1990.01.01' AND '2000.12.31');
+
+SELECT animal_name FROM animals
+JOIN owners ON animals.owners_id = owners.owners_id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT animal_name FROM animals
+JOIN species ON animals.species_id = species.species_id
+WHERE species.species_name = 'Pokemon';
+
+SELECT owners.full_name, animals.animal_name FROM animals
+LEFT JOIN owners ON animals.owners_id = owners.owners_id;
+WHERE animals.owners_id IS NOT NULL AND (animals.owners_id IS NULL);
+
+SELECT species.species_name, COUNT(animals.animal_name) FROM animals
+JOIN species ON animals.species_id = species.species_id
+GROUP BY species.species_name;
+
+SELECT animals.animal_name FROM animals
+JOIN species ON animals.species_id = species.species_id
+WHERE species.species_name = 'Digimon' AND animals.owners_id = 2;
+
+SELECT animal_name FROM animals
+WHERE owners_id = 5 AND escape_attempts = 0;
+
+SELECT owners.full_name, COUNT(animals.animal_name) FROM animals
+JOIN owners ON animals.owners_id = owners.owner_id
+GROUP BY owners.owner_name;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,31 @@
 /* Database schema to keep the structure of entire database. */
-CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals(
-  id INT PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
-  date_of_birth DATE,
-  escape_attempts INT,
-  neutered BOOLEAN,
-  weight_kg DECIMAL
+CREATE TABLE animals (
+ id INT GENERATED ALWAYS AS IDENTITY,
+ name VARCHAR(50),
+ date_of_birth DATE,
+ escape_attempts INT,
+ neutered BOOLEAN DEFAULT false,
+ weight_kg DECIMAL,
+ PRIMARY KEY(id)
 );
 
-ALTER TABLE animals ADD species VARCHAR(50);
+ ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ full_name VARCHAR(50),
+ age INT
+);
+
+CREATE TABLE species(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(50)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
