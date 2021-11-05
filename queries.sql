317fe5b2cc5c2b35effaa26e29e77e26577b4550
codecaiine <akabrouyannickn@gmail.com>
2021-11-05 09:59:26 +0000

    Resolve conflict in data sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,36 @@
-
 /*Queries that provide answers to the questions from all projects.*/
 
+SELECT * FROM animals WHERE name LIKE '%mon';
+
+SELECT name FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered = true;
+
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
 SELECT * FROM animals;
 
 BEGIN;
@@ -53,41 +83,3 @@ SELECT animals.name AS animal_name, animals.date_of_birth, animals.escape_attemp
 SELECT COUNT(visits.animals_id) FROM visits JOIN vets ON vets.id = visits.vets_id JOIN animals ON animals.id = visits.animals_id JOIN specializations ON specializations.vets_id = vets.id WHERE specializations.species_id <> animals.species_id;
 
 SELECT species.name, COUNT(visits.animals_id) as count FROM animals JOIN visits ON animals.id = visits.animals_id JOIN vets ON vets.id = visits.vets_id JOIN species ON species.id = animals.species_id WHERE vets.name = 'Maisy Smith' GROUP BY species.name ORDER BY count DESC LIMIT 1;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth>'2022-01-01';
-SAVEPOINT dateOfBirth;
-UPDATE animals SET weight_kg=-1*weight_kg;
-ROLLBACK TO dateOfBirth;
-UPDATE animals SET weight_kg=-1*weight_kg WHERE weight_kg<0;
-COMMIT;
-
-SELECT COUNT(*) FROM animals;
-SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
-SELECT AVG(weight_kg) FROM animals;
-SELECT neutered, count(neutered) as result FROM animals GROUP BY neutered ORDER BY result DESC LIMIT 1;
-SELECT species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
-
-SELECT animals.name,
-owners.full_name FROM animals JOIN owners ON animals.owners_id = owners.id
-WHERE full_name = 'MelodyPond';
-
-SELECT animals.name AS animal_name,
-species.name AS species_type FROM animals JOIN species ON animals.species_id = species.id
-WHERE species.name='Pokemon';
-
-SELECT full_name,
-animals.name FROM owners LEFT JOIN animals ON animals.owners_id=owners.id;
-
-SELECT count(animals) FROM animals JOIN species ON animals.species_id=species.id;
-
-SELECT animals.name FROM animals JOIN owners ON animals.owners_id=owners.id WHERE owners.full_name='Jennifer Orwell';
-
-SELECT animals.name, 
-animals.escape_attempts FROM animals INNER JOIN owners ON owners.id = animals.owners_id
-  WHERE owners.full_name = 'Dean Winchester'
-  AND animals.escape_attempts = 0;
-
-SELECT owners.full_name, 
-COUNT(animals.name) AS total_animals FROM owners JOIN animals ON animals.owners_id=owners.id GROUP BY owners.full_name ORDER BY total_animals DESC LIMIT 1;
