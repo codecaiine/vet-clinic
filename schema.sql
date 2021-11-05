5aa889bbc2287d8e7342decc9de187379a24d778
codecaiine <akabrouyannickn@gmail.com>
2021-11-05 08:32:21 +0000

    Modify schema sql file

--- schema.sql
+++ schema.sql
@@ -1,32 +1,39 @@
-/* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
- id INT GENERATED ALWAYS AS IDENTITY,
- name VARCHAR(50),
- date_of_birth DATE,
- escape_attempts INT,
- neutered BOOLEAN DEFAULT false,
- weight_kg DECIMAL,
- PRIMARY KEY(id)
+CREATE TABLE animals(
+  id INT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL
 );
 
- ALTER TABLE animals ADD species VARCHAR(50);
+ALTER TABLE animals 
+ADD species TEXT;
 
 CREATE TABLE owners(
- id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
- full_name VARCHAR(50),
- age INT
+  id SERIAL PRIMARY KEY,                    
+  full_name TEXT,   
+  age  INT
 );
-
 
 CREATE TABLE species(
- id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
- name VARCHAR(50)
+  id SERIAL PRIMARY KEY,
+  name TEXT
 );
 
-ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals
+  DROP species;
 
-ALTER TABLE animals ADD species_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
-ALTER TABLE animals ADD owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) 
+REFERENCES species (id);
+
+ALTER TABLE animals 
+ADD COLUMN owners_id INT,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owners_id)
+REFERENCES owners (id);
