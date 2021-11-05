1f2c5f70a5eb0a7bcf1dd66e295518d358a0b835
codecaiine <akabrouyannickn@gmail.com>
2021-11-05 08:42:37 +0000

    Initialize database

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
+
 SELECT name FROM animals WHERE name LIKE '%mon';
 
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
@@ -13,55 +14,3 @@ SELECT name FROM animals WHERE neutered IS TRUE;
 SELECT name FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-
-BEGIN;
-UPDATE animals SET species='unspecified';
-SELECT * FROM animals;
-ROLLBACK;
-
-BEGIN;
-UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species='pokemon' WHERE species IS NULL;
-COMMIT;
-
-BEGIN;
-DELETE FROM animals;
-ROLLBACK;
-
-BEGIN;
-DELETE FROM animals WHERE date_of_birth>'2022-01-01';
-SAVEPOINT dateOfBirth;
-UPDATE animals SET weight_kg=-1*weight_kg;
-ROLLBACK TO dateOfBirth;
-UPDATE animals SET weight_kg=-1*weight_kg WHERE weight_kg<0;
-COMMIT;
-
-SELECT COUNT(*) FROM animals;
-SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
-SELECT AVG(weight_kg) FROM animals;
-SELECT neutered, count(neutered) as result FROM animals GROUP BY neutered ORDER BY result DESC LIMIT 1;
-SELECT species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight FROM animals GROUP BY species;
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
-
-SELECT animals.name,
-owners.full_name FROM animals JOIN owners ON animals.owners_id = owners.id
-WHERE full_name = 'MelodyPond';
-
-SELECT animals.name AS animal_name,
-species.name AS species_type FROM animals JOIN species ON animals.species_id = species.id
-WHERE species.name='Pokemon';
-
-SELECT full_name,
-animals.name FROM owners LEFT JOIN animals ON animals.owners_id=owners.id;
-
-SELECT count(animals) FROM animals JOIN species ON animals.species_id=species.id;
-
-SELECT animals.name FROM animals JOIN owners ON animals.owners_id=owners.id WHERE owners.full_name='Jennifer Orwell';
-
-SELECT animals.name, 
-animals.escape_attempts FROM animals INNER JOIN owners ON owners.id = animals.owners_id
-  WHERE owners.full_name = 'Dean Winchester'
-  AND animals.escape_attempts = 0;
-
-SELECT owners.full_name, 
-COUNT(animals.name) AS total_animals FROM owners JOIN animals ON animals.owners_id=owners.id GROUP BY owners.full_name ORDER BY total_animals DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -8,32 +8,3 @@ CREATE TABLE animals(
   neutered BOOLEAN,
   weight_kg DECIMAL
 );
-
-ALTER TABLE animals 
-ADD species TEXT;
-
-CREATE TABLE owners(
-  id SERIAL PRIMARY KEY,                    
-  full_name TEXT,   
-  age  INT
-);
-
-CREATE TABLE species(
-  id SERIAL PRIMARY KEY,
-  name TEXT
-);
-
-ALTER TABLE animals
-  DROP species;
-
-ALTER TABLE animals
-ADD COLUMN species_id INT,
-ADD CONSTRAINT fk_species
-FOREIGN KEY (species_id) 
-REFERENCES species (id);
-
-ALTER TABLE animals 
-ADD COLUMN owners_id INT,
-ADD CONSTRAINT fk_owners
-FOREIGN KEY (owners_id)
-REFERENCES owners (id);
