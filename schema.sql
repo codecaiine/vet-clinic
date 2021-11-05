88d35bf44489ad6050218c0231d524d44510764a
codecaiine <akabrouyannickn@gmail.com>
2021-11-05 08:47:45 +0000

    Modify schema sql

--- schema.sql
+++ schema.sql
@@ -8,3 +8,32 @@ CREATE TABLE animals(
   neutered BOOLEAN,
   weight_kg DECIMAL
 );
+
+ALTER TABLE animals 
+ADD species TEXT;
+
+CREATE TABLE owners(
+  id SERIAL PRIMARY KEY,                    
+  full_name TEXT,   
+  age  INT
+);
+
+CREATE TABLE species(
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+ALTER TABLE animals
+  DROP species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) 
+REFERENCES species (id);
+
+ALTER TABLE animals 
+ADD COLUMN owners_id INT,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owners_id)
+REFERENCES owners (id);
