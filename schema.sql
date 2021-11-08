ee5d163ad37b1c2b11973b750afb898c9dce426a
codecaiine <akabrouyannickn@gmail.com>
2021-11-08 22:50:21 +0000

    Decrease the execution time of the second query

--- schema.sql
+++ schema.sql
@@ -58,11 +58,17 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
+
 -- Find a way to decrease the execution time of the first query. Look for hints in the previous lessons.
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animalS_id = 4;
 CREATE INDEX visits_animals_id_asc ON visits(animals_id ASC);
 \d visits
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animalS_id = 4;
+
+
+-- Decrease the execution time of the first query.
+CREATE INDEX visits_visits_id_asc ON visits(vets_id ASC);
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vets_id = 2;
 
 -- Decrease he execution time of the third query
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
