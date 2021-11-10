75ceef123bf443066fe1faaa412858ed9a1e41e6
codecaiine <akabrouyannickn@gmail.com>
2021-11-10 11:23:24 +0000

    Modify schema based on diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,39 +1,48 @@
 CREATE TABLE patients(
   id INT PRIMARY KEY NOT NULL,
-  name TEXT NOT NULL,
+  name VARCHAR(225) NOT NULL,
   date_of_birth DATE,
 );
 
-CREATE TABLE medical_histories(
+CREATE TABLE medical_history(
   id INT PRIMARY KEY NOT NULL,
   admitted_at TIMESTAMP,
   patient_id INT, 
   status VARCHAR(255),
-  CONSTRAINT fk_patients FOREIGN KEY(patients_id) REFERENCES patients(id),
-  CONSTRAINT fk_treatments FOREIGN KEY(treatments_id) REFERENCES treatments(id),
-);
+  CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id),
+  );
 
 CREATE TABLE treatments(
   id INT PRIMARY KEY NOT NULL,
   type VARCHAR(255), 
-  name TEXT NOT NULL,
-  CONSTRAINT fk_invoice_items FOREIGN KEY(invoice_items_id) REFERENCES invoice_items(id),
+  name VARCHAR(225),
+  CONSTRAINT fk_invoice_items FOREIGN KEY(invoice_item_id) REFERENCES invoice_items(id),
 );
 
 CREATE TABLE invoices(
   id INT PRIMARY KEY NOT NULL,
-  total_amount DECIMAL,
+  total_amount FLOAT,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
-  CONSTRAINT fk_medical_histories FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id),
+  medical_history_id INT,
+  CONSTRAINT fk_medical_histories FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
 );
 
 CREATE TABLE invoice_items(
   id INT PRIMARY KEY NOT NULL,
-  unit_price DECIMAL,
+  unit_price FLOAT,
   quantity INT,
-  total_price DECIMAL,
+  total_price FLOAT,
   invoice_id INT,
   treatment_id INT,
-  CONSTRAINT fk_invoices FOREIGN KEY(invoices_id) REFERENCES invoices(id),
+  CONSTRAINT fk_invoice FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+  CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id),
 );
+
+CREATE TABLE treatments_histories (
+  id INT PRIMARY KEY NOT NULL,
+  treatment_id INT,
+  medical_history_id INT;
+  CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+  CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+)
