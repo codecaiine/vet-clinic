e16ea56526d1adf3a6af47a343df7dae1d56b66b
codecaiine <akabrouyannickn@gmail.com>
2021-11-10 13:51:26 +0000

    Update schema

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,48 +1,54 @@
-CREATE TABLE patients(
-  id INT PRIMARY KEY NOT NULL,
-  name VARCHAR(225) NOT NULL,
-  date_of_birth DATE,
-);
+CREATE TABLE patients (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  date_of_birth date
+)
 
-CREATE TABLE medical_history(
-  id INT PRIMARY KEY NOT NULL,
-  admitted_at TIMESTAMP,
-  patient_id INT, 
-  status VARCHAR(255),
-  CONSTRAINT fk_patients FOREIGN KEY(patient_id) REFERENCES patients(id),
-  );
-
-CREATE TABLE treatments(
-  id INT PRIMARY KEY NOT NULL,
-  type VARCHAR(255), 
-  name VARCHAR(225),
-  CONSTRAINT fk_invoice_items FOREIGN KEY(invoice_item_id) REFERENCES invoice_items(id),
-);
-
-CREATE TABLE invoices(
-  id INT PRIMARY KEY NOT NULL,
-  total_amount FLOAT,
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL,
   generated_at TIMESTAMP,
   payed_at TIMESTAMP,
   medical_history_id INT,
-  CONSTRAINT fk_medical_histories FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
-);
+  CONSTRAINT fk_medical_history
+  FOREIGN KEY (medical_history_id) 
+  REFERENCES medical_histories(id)
+)
 
-CREATE TABLE invoice_items(
-  id INT PRIMARY KEY NOT NULL,
-  unit_price FLOAT,
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
   quantity INT,
-  total_price FLOAT,
+  total_price DECIMAL,
   invoice_id INT,
   treatment_id INT,
-  CONSTRAINT fk_invoice FOREIGN KEY(invoice_id) REFERENCES invoices(id),
-  CONSTRAINT fk_treatment FOREIGN KEY(treatment_id) REFERENCES treatments(id),
-);
-
-CREATE TABLE treatments_histories (
-  id INT PRIMARY KEY NOT NULL,
-  treatment_id INT,
-  medical_history_id INT;
-  CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id),
-  CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  CONSTRAINT fk_invoice 
+  FOREIGN KEY (invoice_id) 
+  REFERENCES invoices(id),
+  CONSTRAINT fk_treatment 
+  FOREIGN KEY (treatment_id) 
+  REFERENCES treatments(id)
 )
+
+CREATE TABLE medical_history (
+  id SERIAL PRIMARY KEY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR(255),
+  CONSTRAINT fk_patient
+  FOREIGN KEY(patient_id)
+  REFERENCES patients(id)
+)
+
+CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(255),
+  name VARCHAR(255),
+)
+
+CREATE TABLE treatments_history (
+  treatment_id INT,
+  medical_history_id INT,
+  CONSTRAINT fk_tr FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
+  CONSTRAINT fk_mh FOREIGN KEY(medical_history_id) REFERENCES medical_history(id) ON DELETE CASCADE
+);
